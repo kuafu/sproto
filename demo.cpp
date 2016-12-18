d8fd5a6eed7d15b892d3de38c39b29419b8d40fd
TerraNova <augustus4400@gmail.com>
2016-12-18 14:55:31 +0800

    m

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <fstream>
 
+// #include "lua.h"
+// #include "lauxlib.h"
+extern "C"
+{
+#include "lua.h"
+
+#include "luaconf.h"
+
+#include "lauxlib.h"
+#include "lualib.h"
+
+}
+
 bool LoadPbfile(const char* filename, std::string& pb)
 {
 	std::ifstream ifs(filename, std::ifstream::binary);
@@ -30,8 +43,36 @@ bool SaveMsgfile(const char* filename, const char* msg, size_t len)
 	return true;
 }
 
+// for skynet_lalloc use
+#define raw_realloc realloc
+#define raw_free free
+
+
+void* skynet_lalloc(void *ud, void *ptr, size_t osize, size_t nsize) {
+	if(nsize == 0)
+	{
+		raw_free(ptr);
+		return NULL;
+	}
+	else
+	{
+		return raw_realloc(ptr, nsize);
+	}
+}
+
 int main(int argc, char* argv[])
 {
+	lua_State *L = lua_newstate(skynet_lalloc, NULL);
+
+	lua_getglobal(L, "f");		/* function to be called */
+	lua_pushliteral(L, "how");	/* 1st argument */
+	lua_getglobal(L, "t");		/* table to be indexed */
+	lua_getfield(L, -1, "x");	/* push result of t.x (2nd arg) */
+	lua_remove(L, -2);			/* remove 't' from the stack */
+	lua_pushinteger(L, 14);		/* 3rd argument */
+	lua_call(L, 3, 1);			/* call 'f' with 3 arguments and 1 result */
+	lua_setglobal(L, "a");		/* set global 'a' */
+
 	std::string pb;
 	if (!LoadPbfile("addressbook.pb", pb))
 	{
@@ -42,3 +83,12 @@ int main(int argc, char* argv[])
 
 }
 
+// void test
+// {
+// 	lua_State *L = lua_State();
+// 	printf("%d\n", lua_gettop(L));
+// 	lua_dostring(L, "return 1,'a'");
+// 	printf("%d\n", lua_gettop(L));
+// 	printf("%s\n", lua_tostring(L,-2));
+// 	printf("%s\n", lua_tostring(L,-1));
+// }
